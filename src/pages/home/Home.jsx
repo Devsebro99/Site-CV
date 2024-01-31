0927c54dddbec6e83fc8be8f881b106d5b512384
Devsebro99 <100680925+Devsebro99@users.noreply.github.com>
2024-01-31 16:31:58 +0100

    premiere page réalisée

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,23 +8,39 @@ import BottomPage from '../../layouts/footer/Footer.jsx'
 export default function Presentation() {
   const datas = useState(Datas[0])
   const TitleMaster = datas[0].title
-  const allSections = datas[0].alllink
-  const linksArray = allSections.map((item) => item.link || null)
-  const titlesArray = allSections.map((item) => item.title || null)
-  const parafsArray = allSections.map((item) => item.paraf)
+  const allCards = datas[0].alllink
+  const linksArray = allCards.map((item) => item.link || null)
+  const titlesArray = allCards.map((item) => item.title || null)
+  const parafsArray = allCards.map((item) => item.paraf || null)
+  const lastIndex = linksArray.length - 1
 
-  const card = linksArray.map((link, index) => (
-    <article className="oneCard" key={index}>
-      <Link className="address" to={link}>
-        <h2 className="btn">{titlesArray[index]}</h2>
+  const card = linksArray.map(
+    (link, index) =>
+      index > 0 && (
+        <article className="oneCard" key={index}>
+          {console.log(link)}
+          {console.log(index)}
+          <Link className="address" to={link}>
+            <h2 className="btn">{titlesArray[index - 1]}</h2>
+          </Link>
+          <div>
+            {parafsArray[index - 1].map((txt, paraIndex) => (
+              <p key={paraIndex}>{txt}</p>
+            ))}
+          </div>
+        </article>
+      )
+  )
+
+  const lastCard = (
+    <article className="oneCard" key={lastIndex}>
+      <Link className="oneCard_title" to={linksArray[lastIndex]}>
+        <h2>{titlesArray[lastIndex]}</h2>
       </Link>
-      <div>
-        {parafsArray[index].map((txt, paraIndex) => (
-          <p key={paraIndex}>{txt}</p>
-        ))}
-      </div>
+      <div>{parafsArray[lastIndex]}</div>
     </article>
-  ))
+  )
+
   return (
     <main id="siteMain">
       <div className="banner">
@@ -37,7 +53,8 @@ export default function Presentation() {
       <section className="containerCenter">
         <nav className="containerCenter_multiCard">{card}</nav>
         <figure className="containerCenter_image">
-          <div className="containerCenter_imageChildren"></div>
+          <div className="containerCenter_image_picture"></div>
+          <div className="containerCenter_image_cardAnnex">{lastCard}</div>
         </figure>
       </section>
       <BottomPage></BottomPage>
